a386dd59e222cb2f290a12c79d877722037f6b65
Ashish Sahu <itsashishsahu.inc@gmail.com>
2021-11-27 01:06:25 +0530

    JavaScript Is Updated.

---
+++ script.js
@@ -0,0 +1,66 @@
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
+
+
+let synth = speechSynthesis;
+isSpeaking = true;
+
+voices();
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        
+        // selecting "Google US English" voice as default 
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        //creating an option tag with passing voice name and voice languge
+        let option = `<option value ="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`
+        voiceList.insertAdjacentHTML("beforeend", option); //inserting option tag beforeend of select tag
+    }
+}
+
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text){
+    let utternance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        // if the available device voice name is equal to the user selected voice name 
+        // then set the speech voice to the user selected voice 
+      if(voice.name === voiceList.value){
+        utternance.voice = voice;
+      }  
+    }
+    synth.speak(utternance); //speak the speech / utternance 
+}
+
+speechBtn.addEventListener("click", e =>  {
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+        }
+        if(textarea.value.length > 80){
+            if (isSpeaking){
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            }else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+
+
+            //
+            setInterval(() => {
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Speak";
+                }
+            });
+        } else{
+            speechBtn.innerText = "Speak";
+        }
+    }
+});
